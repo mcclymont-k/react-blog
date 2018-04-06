540bef13c3d8d9543f28c6e2f6ed96d340a37d41
Kieran McClymont <mcclymont.kieran@gmail.com>
2018-04-06 10:46:50 -0300

    Updated the gallery, it is now responsive to click and responsive to page size.

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -9,7 +9,7 @@ class Gallery extends Component {
 
   componentDidMount() {
     const digits = Array.from({ length: 50}, () =>
-      [this.randomNumber(4), this.randomNumber(4)]).concat([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]])
+      [this.randomNumber(4), this.randomNumber(4)]).concat([[1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]])
     this.setState({digits: digits})
   }
 
@@ -17,23 +17,41 @@ class Gallery extends Component {
     return Math.floor(Math.random() * limit) + 1
   }
 
+  handleClick(e) {
+    let currentImage = e.currentTarget.querySelector('img').src
+    let overlay = document.querySelector('.mainOverlay')
+    let overlayImage = overlay.querySelector('img')
+    overlayImage.src = currentImage
+    overlay.classList.add('mainOverlayOpen')
+  }
 
-
+  close() {
+    let overlay = document.querySelector('.mainOverlay')
+    console.log(overlay)
+    console.log(overlay.classList)
+    overlay.classList.remove('mainOverlayOpen')
+  }
 
   render () {
     return(
-      <div className='galleryContainer'>
-        {
-          this.state.digits
-          ? this.state.digits.map(([h, v]) =>
-            <div className= {`item  h${h} v${v}`}>
-              <img src={require(`../Images/${this.randomNumber(8)}.jpg`)} />
-              <div className='itemOverlay'>
-                <button>View ></button>
-              </div>
+      <div>
+        <div className='mainOverlay' onClick={this.close}>
+          <button onClick={this.close} className='imageCloseButton'>Close x</button>
+          <img className='overlayImage' />
+        </div>
+        <div className='galleryContainer'>
+          {
+            this.state.digits
+            ? this.state.digits.map(([h, v]) =>
+              <div className= {`item  h${h} v${v}`} onClick={this.handleClick}>
+                <img src={require(`../Images/${this.randomNumber(12)}.jpg`)} />
+                <div className='itemOverlay'>
+                  <button className="itemOverlayButton">View ></button>
+                </div>
             </div>
           ): ""
         }
+        </div>
       </div>
     )
   }
